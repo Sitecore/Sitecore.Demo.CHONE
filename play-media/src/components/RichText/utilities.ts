9197c764641b8567b6359e37bfe033ec5a209eb2
Jeff L'Heureux <2000200+jflheureux@users.noreply.github.com>
2022-12-19 11:19:36 -0500

    Fix console error about RichText children element keys (#2)

--- play-media/src/components/RichText/utilities.ts
+++ play-media/src/components/RichText/utilities.ts
@@ -3,18 +3,22 @@ import { RichTextResponseItem } from './types/response';
 
 const getComponentFromChild = (
   child: any,
-  componentMap: Record<string, FunctionComponent>
+  componentMap: Record<string, FunctionComponent>,
+  ...indexes: number[]
 ): ReactNode => {
+  const key = indexes.join('_');
+
   if (child?.content?.length) {
     return componentMap[child.type](
       child,
-      child.content.map((childContext: any) => getComponentFromChild(childContext, componentMap))
+      child.content.map((childContext: any, childIndex: number) => getComponentFromChild(childContext, componentMap, ...indexes, childIndex)),
+      key
     );
   }
 
-  return componentMap[child.type](child);
+  return componentMap[child.type](child, key);
 };
 
 export const getComponentTree = (content: any, componentMap: any) => {
-  return content.map((element: any) => getComponentFromChild(element, componentMap));
+  return content.map((element: any, index: number) => getComponentFromChild(element, componentMap, index));
 };
