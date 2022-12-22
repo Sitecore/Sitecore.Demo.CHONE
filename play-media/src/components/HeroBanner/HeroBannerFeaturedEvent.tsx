ce99832c0877769a572e03b97d6091105f2a1561
Christos Papoutsakis <chpa@sitecore.net>
2022-12-22 17:49:12 +0200

    Update event links to use the new format

--- play-media/src/components/HeroBanner/HeroBannerFeaturedEvent.tsx
+++ play-media/src/components/HeroBanner/HeroBannerFeaturedEvent.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { EVENT_MOCK_IMG } from '../../constants/mockImages';
 import { getEventSchedule } from '../../helpers/eventScheduleHelper';
+import slugify from 'slugify';
 import { Event } from '../../interfaces/event';
 import { ShadowBox } from '../Common/ShadowBox';
 import { HeroBanner } from './HeroBanner';
@@ -11,7 +12,7 @@ export const HeroBannerFeaturedEvent = ({ event }: { event: Event }) => {
       imageSrc={event?.featuredImage?.results[0]?.fileUrl || EVENT_MOCK_IMG}
       className="hero-banner-featured-event"
     >
-      <Link href={`event/${event?.slug}`}>
+      <Link href={`event/${event.id}/${slugify(event?.title, { lower: true })}`}>
         <ShadowBox
           color={event?.sport?.results[0]?.color}
           label={event?.sport?.results[0]?.title}
