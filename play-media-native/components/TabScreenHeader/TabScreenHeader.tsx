869d60780a957c1b8dc0fc7b3e96cfa113dd3e88
Neli Kostadinova <nko@sitecore.net>
2023-01-09 17:02:50 +0200

    Some functionality for tab nav

--- play-media-native/components/TabScreenHeader/TabScreenHeader.tsx
+++ play-media-native/components/TabScreenHeader/TabScreenHeader.tsx
@@ -1,12 +1,19 @@
 import { View } from "react-native";
 import { Logo } from "../Logo/Logo";
-import { Button } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 import { theme } from "../../theme/theme";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faFilter, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-export const TabScreenHeader = () => {
+type TabScreenHeaderProps = {
+  navigation: any;
+  type: "Event" | "Athlete";
+};
+
+export const TabScreenHeader = ({ navigation, type }: TabScreenHeaderProps) => {
+  const [filtersOpen, setFiltersOpen] = useState(false);
   const insets = useSafeAreaInsets();
 
   return (
@@ -28,6 +35,7 @@ export const TabScreenHeader = () => {
         }}
       >
         <Button
+          mode="outlined"
           icon={({ size, color }) => <FontAwesomeIcon icon={faFilter} color={color} size={size} />}
           labelStyle={{
             fontFamily: theme.fontFamily.medium,
@@ -36,7 +44,9 @@ export const TabScreenHeader = () => {
           }}
           style={{
             marginRight: theme.spacing.xs,
+            borderColor: filtersOpen ? theme.colors.yellow.DEFAULT : theme.colors.transparent,
           }}
+          onPress={() => setFiltersOpen(!filtersOpen)}
         >
           Filter
         </Button>
@@ -48,10 +58,17 @@ export const TabScreenHeader = () => {
             fontSize: theme.fontSize.base,
             lineHeight: 30,
           }}
+          // to align with outline button
+          style={{
+            borderWidth: 1,
+            borderColor: theme.colors.yellow.DEFAULT,
+          }}
+          onPress={() => navigation.navigate(`Add${type}`)}
         >
-          Event
+          {type}
         </Button>
       </View>
+      {filtersOpen && <Text>Filters go here</Text>}
     </View>
   );
 };
