aa67fa14ee07c51a4a763bd1fe93dd6f71de742e
Neli Kostadinova <nko@sitecore.net>
2023-01-11 15:47:17 +0200

    Infinite scroll and refresh on listings

--- play-media-native/components/Listing/Listing.tsx
+++ play-media-native/components/Listing/Listing.tsx
@@ -1,8 +1,53 @@
-import { FlatList, SafeAreaView } from "react-native";
-import { Text } from "react-native-paper";
+import { useCallback, useEffect, useState } from "react";
+import { FlatList, RefreshControl, SafeAreaView, View } from "react-native";
+import { Text, ActivityIndicator } from "react-native-paper";
+import { MOCK_FETCH_TIMEOUT, PAGE_SIZE } from "../../constants/pagination";
+import { mockFetchData } from "../../helpers/mockPagination";
 import { theme } from "../../theme/theme";
 
 export const Listing = ({ data, isLoading, renderItem }) => {
+  const [items, setItems] = useState(data?.slice(0, PAGE_SIZE));
+  const [loading, setLoading] = useState(false);
+  const [isListEnd, setIsListEnd] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    setItems(data?.slice(0, PAGE_SIZE));
+  }, [data]);
+
+  useEffect(() => {
+    setIsListEnd(data && items && data?.length === items?.length);
+  }, [items]);
+
+  const fetchMoreData = useCallback(() => {
+    !isListEnd && mockFetchData(data, items, setItems, setLoading);
+  }, [data, items, isListEnd]);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setItems(data?.slice(0, PAGE_SIZE));
+      setIsListEnd(false);
+      setRefreshing(false);
+    }, MOCK_FETCH_TIMEOUT);
+  }, [data]);
+
+  const ListFooter = (
+    <>
+      {loading && <ActivityIndicator animating={loading} />}
+      {isListEnd && (
+        <Text
+          style={{
+            textAlign: "center",
+            color: theme.colors.gray.DEFAULT,
+          }}
+        >
+          No more items to show at the moment
+        </Text>
+      )}
+    </>
+  );
+
   if (isLoading) {
     return (
       <SafeAreaView
@@ -11,21 +56,42 @@ export const Listing = ({ data, isLoading, renderItem }) => {
           justifyContent: "center",
           alignItems: "center",
           height: "100%",
-          backgroundColor: "#000",
+          backgroundColor: theme.colors.black.darkest,
         }}
       >
-        <Text>Loading ....</Text>
+        <ActivityIndicator animating={true} size="large" />
       </SafeAreaView>
     );
   }
 
   return (
     <SafeAreaView style={{ backgroundColor: theme.colors.black.darkest }}>
+      <View
+        style={{
+          position: "absolute",
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <ActivityIndicator animating={refreshing} size="small" />
+      </View>
       <FlatList
         style={{ paddingHorizontal: theme.spacing.sm }}
-        data={data}
+        data={items}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
+        onEndReachedThreshold={0.2}
+        onEndReached={fetchMoreData}
+        ListFooterComponent={ListFooter}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={[theme.colors.transparent]}
+            tintColor={theme.colors.transparent}
+          />
+        }
       />
     </SafeAreaView>
   );
