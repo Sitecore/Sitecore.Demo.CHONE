297e9b8ac6bc6abba2b41a45c082c44579ff2e8f
Christos Papoutsakis <chpa@sitecore.net>
2023-01-27 15:05:54 +0200

    Add generateToken method for authentication

---
+++ play-media-native/api/queries/generateToken/index.ts
@@ -0,0 +1,39 @@
+import { FetchOptions } from "../../../interfaces/fetchOptions";
+import { Token } from "../../../interfaces/token";
+import { store } from "../../../store";
+
+export const generateToken = async (options?: FetchOptions): Promise<Token> => {
+  const details = {
+    client_id:
+      options.clientID ||
+      store.getState().connections.selectedConnection?.clientID,
+    client_secret:
+      options.clientSecret ||
+      store.getState().connections.selectedConnection?.clientSecret,
+    audience: "https://api.sitecorecloud.io",
+    grant_type: "client_credentials",
+  };
+
+  const formBody = Object.keys(details)
+    .map(
+      (key) => encodeURIComponent(key) + "=" + encodeURIComponent(details[key])
+    )
+    .join("&");
+
+  try {
+    return await fetch("https://auth.sitecorecloud.io/oauth/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: formBody,
+    }).then(async (response: Response) => {
+      const jsonResponsePromise = response.json();
+      const data = (await jsonResponsePromise) as Token;
+
+      return data;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
