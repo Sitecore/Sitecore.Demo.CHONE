6974e5afa25469fc272e2c71b44a3f42e3ed6d4e
Christos Papoutsakis <chpa@sitecore.net>
2023-02-08 18:56:12 +0200

    Add updateContentItem function

--- play-media-native/api/queries/contentItems/index.ts
+++ play-media-native/api/queries/contentItems/index.ts
@@ -1,16 +1,49 @@
-import { ContentItem } from "../../../interfaces/contentItem";
+import {
+  ContentItemCreate,
+  ContentItemUpdate,
+} from "../../../interfaces/contentItem";
 import { generateToken } from "../generateToken";
 
 const apiURL = "https://content-api.sitecorecloud.io/api/content/v1/items";
 
 export const createContentItem = async (
-  contentItem: ContentItem
+  contentItem: ContentItemCreate
 ): Promise<unknown> => {
   const accessToken: string = (await generateToken()).access_token;
 
   try {
     return await fetch(apiURL, {
       method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        Accept: "text/plain",
+        "Content-Type": "application/json-patch+json",
+      },
+      body: JSON.stringify(contentItem),
+    }).then(async (response: Response) => {
+      const jsonResponsePromise = response.json();
+      const data = await jsonResponsePromise;
+
+      if (data?.error) {
+        console.error(data?.error_description);
+        throw data.error;
+      }
+
+      return data;
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateContentItem = async (
+  contentItem: ContentItemUpdate
+): Promise<unknown> => {
+  const accessToken: string = (await generateToken()).access_token;
+
+  try {
+    return await fetch(`${apiURL}/${contentItem.id}`, {
+      method: "PUT",
       headers: {
         Authorization: `Bearer ${accessToken}`,
         Accept: "text/plain",
