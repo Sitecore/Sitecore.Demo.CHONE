7abd9fd1cc1e64b993562af6d9455fc7bf07d1a4
Christos Papoutsakis <chpa@sitecore.net>
2023-02-08 15:27:55 +0200

    Add ContentItemUpdate interface

--- play-media-native/interfaces/contentItem/index.ts
+++ play-media-native/interfaces/contentItem/index.ts
@@ -1,7 +1,17 @@
-export interface ContentItem {
+export interface ContentItemCreate {
   id?: string;
   name: string;
   contentTypeId: string;
+  fields: {
+    [prop: string]: {
+      value?: unknown;
+    };
+  };
+}
+
+export interface ContentItemUpdate {
+  id: string;
+  name: string;
   fields: {
     [prop: string]: {
       value?: unknown;
