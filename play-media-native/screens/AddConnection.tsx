a09fe306d4aa8ac64aecc609c13a6021fc704a21
Christos Papoutsakis <chpa@sitecore.net>
2023-02-13 16:06:27 +0200

    Fix styling and text on add connection screen

--- play-media-native/screens/AddConnection.tsx
+++ play-media-native/screens/AddConnection.tsx
@@ -1,28 +1,34 @@
-import { StatusBar, View } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 import { FormAddConnection } from "../features/FormAddConnection/FormAddConnection";
 import { KeyboardAwareScreen } from "../features/Screen/KeyboardAwareScreen";
 import { theme } from "../theme/theme";
 
+const pageStyles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing.lg,
+  },
+  title: {
+    maxWidth: "80%",
+    textAlign: "center",
+  },
+  chOneText: {
+    fontFamily: theme.fontFamily.bold,
+  },
+});
+
 export const AddConnectionScreen = () => {
   return (
     <KeyboardAwareScreen centered>
       <StatusBar barStyle={"light-content"} />
-      <View
-        style={{
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: theme.spacing.lg,
-        }}
-      >
-        <Text
-          style={{
-            maxWidth: "80%",
-            textAlign: "center",
-          }}
-        >
-          Add a connection to a Content Hub One instance.
+      <View style={pageStyles.container}>
+        <Text style={pageStyles.title}>
+          Connect to a
+          <Text style={pageStyles.chOneText}> Content Hub ONE </Text>
+          instance via:
         </Text>
       </View>
       <FormAddConnection />
