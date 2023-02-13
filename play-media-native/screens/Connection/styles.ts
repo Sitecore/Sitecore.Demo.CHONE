0160b9fb5cea356992a345eda4655703fe8293ec
Christos Papoutsakis <chpa@sitecore.net>
2023-02-13 17:27:51 +0200

    Add styles for connection screens

---
+++ play-media-native/screens/Connection/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../theme/theme";
+
+const connectionScreenStyles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing.lg,
+  },
+  title: {
+    maxWidth: "80%",
+    textAlign: "center",
+  },
+  chOneText: {
+    fontFamily: theme.fontFamily.bold,
+  },
+  actionBtns: {
+    paddingBottom: 0,
+    paddingRight: theme.spacing.xs,
+  },
+});
+
+export default connectionScreenStyles;
