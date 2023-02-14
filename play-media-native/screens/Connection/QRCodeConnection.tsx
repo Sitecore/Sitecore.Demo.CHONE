1e7f12bb7c0748986c43bca60de5e3635f91702a
Christos Papoutsakis <chpa@sitecore.net>
2023-02-14 18:22:04 +0200

    Update QR code connection screen with expo barcode scanner

--- play-media-native/screens/Connection/QRCodeConnection.tsx
+++ play-media-native/screens/Connection/QRCodeConnection.tsx
@@ -1,9 +1,52 @@
-import { Text } from "react-native-paper";
+import { useState, useEffect, useCallback } from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Screen } from "../../features/Screen/Screen";
+
+const pageStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
 
 export const QRCodeConnectionScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [isQRScanned, setIsQRScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+
+      if (status === "granted") {
+        setHasPermission(true);
+      } else {
+        setHasPermission(false);
+      }
+    })();
+  }, []);
+
+  const handleQRCodeScan = useCallback(({ type, data }) => {
+    setIsQRScanned(true);
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <Screen centered>
+        <Text>No camera permission granted</Text>
+      </Screen>
+    );
+  }
+
   return (
-    <>
-      <Text>QR code connection screen</Text>
-    </>
+    <View style={pageStyles.container}>
+      <BarCodeScanner
+        barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+        onBarCodeScanned={isQRScanned ? undefined : handleQRCodeScan}
+        style={StyleSheet.absoluteFillObject}
+      />
+    </View>
   );
 };
