9b982dd1f03ec7e2dfc8dce1944e245fd9e7a78b
Christos Papoutsakis <chpa@sitecore.net>
2023-02-14 14:05:27 +0200

    Redirect to athlete listing on success

--- play-media-native/screens/AthleteDetail.tsx
+++ play-media-native/screens/AthleteDetail.tsx
@@ -104,7 +104,6 @@ export const AthleteDetailScreen = ({ route, navigation }) => {
   const [shouldShowBottomActions, setShouldShowBottomActions] = useState(true);
 
   const [newAthleteID, setNewAthleteID] = useState(undefined);
-  const [newAthleteName, setNewAthleteName] = useState("");
 
   const isReview = route.params.isReview;
   const isNewAthlete = route.params.isNewAthlete;
@@ -146,17 +145,16 @@ export const AthleteDetailScreen = ({ route, navigation }) => {
 
   const processResponse = useCallback((res: { id: string; name: string }) => {
     setNewAthleteID(res.id);
-    setNewAthleteName(res.name);
     setShowSuccessToast(true);
   }, []);
 
   const handleSuccessToastDismiss = useCallback(() => {
     setShowSuccessToast(false);
-    navigation.navigate("AthleteDetail", {
+    navigation.navigate("MainTabs", {
+      screen: "Athletes",
       id: newAthleteID,
-      title: newAthleteName,
     });
-  }, [newAthleteID, newAthleteName]);
+  }, [newAthleteID]);
 
   const handleErrorToastDismiss = useCallback(() => {
     setShowErrorToast(false);
