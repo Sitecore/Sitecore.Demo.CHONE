2ffc342effdbb389fe065915e7cfcff8dc0ce2dc
Christos Papoutsakis <chpa@sitecore.net>
2023-03-09 20:05:55 +0200

    Add publishAthlete helper function

--- play-media-native/helpers/athletes.ts
+++ play-media-native/helpers/athletes.ts
@@ -1,3 +1,5 @@
+import { publishContentItem } from '../api/queries/contentItems';
+import { publishMediaItem } from '../api/queries/mediaItems';
 import { ATHLETE_FACETS } from '../constants/filters';
 import { Athlete } from '../interfaces/athlete';
 import { Sport } from '../interfaces/sport';
@@ -18,4 +20,30 @@ export const initializeAthletes = (athletes: Athlete[], sports: Sport[]) => {
 export const removeAlreadySelected = (athletes: Athlete[], existingAthletes: Athlete[]) => {
   const existingAthleteIDs = existingAthletes.map((item) => item.id);
   return athletes.filter((athlete) => !existingAthleteIDs.includes(athlete.id));
+};
+
+// Helper function to iterate over an athlete and publish all related media
+// (profilePhoto, featuredImage, relatedMedia) and the athlete object
+export const publishAthlete = async (athlete: Athlete): Promise<unknown> => {
+  // If the athlete has a profile photo and/ or a featured image and/ or related media create a promise for each one of them
+  const mediaPromises = [];
+  if (athlete?.profilePhoto?.id) {
+    mediaPromises.push(() => publishMediaItem(athlete.profilePhoto.id));
+  }
+  if (athlete?.featuredImage?.id) {
+    mediaPromises.push(() => publishMediaItem(athlete.featuredImage.id));
+  }
+  if (athlete?.relatedMedia?.length > 0) {
+    for (const mediaItem of athlete.relatedMedia) {
+      if (mediaItem?.id) {
+        mediaPromises.push(() => publishMediaItem(mediaItem.id));
+      }
+    }
+  }
+
+  // Run all of the above promises in parallel along with the one for publishing the athlete
+  return await Promise.all([
+    ...mediaPromises.map((mediaPromise) => mediaPromise()),
+    publishContentItem(athlete.id),
+  ]);
 };
