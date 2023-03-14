ce6ea2da87bd49bc38f4acdbf269fc7ef8a211b6
Christos Papoutsakis <chpa@sitecore.net>
2023-03-14 17:24:17 +0200

    Remove redundant catch clause

--- play-media-native/api/index.ts
+++ play-media-native/api/index.ts
@@ -23,23 +23,19 @@ export async function fetchGraphQL(query: string, options?: FetchOptions): Promi
         'X-GQL-Token': apiKey,
       },
       body: JSON.stringify({ query }),
-    })
-      .then((response: Response) => {
-        const jsonResponsePromise = response.json();
-        jsonResponsePromise.then((jsonResponse: unknown) => {
-          const responseWithErrors = jsonResponse as GraphQLResponseWithErrors;
-          if (responseWithErrors.errors && responseWithErrors.errors.length > 0) {
-            console.error(
-              'An error was returned by a GraphQL query. See the associated logged object for details.',
-              responseWithErrors
-            );
-          }
-        });
-        return jsonResponsePromise;
-      })
-      .catch((error) => {
-        return console.log(error);
+    }).then((response: Response) => {
+      const jsonResponsePromise = response.json();
+      jsonResponsePromise.then((jsonResponse: unknown) => {
+        const responseWithErrors = jsonResponse as GraphQLResponseWithErrors;
+        if (responseWithErrors.errors && responseWithErrors.errors.length > 0) {
+          console.error(
+            'An error was returned by a GraphQL query. See the associated logged object for details.',
+            responseWithErrors
+          );
+        }
       });
+      return jsonResponsePromise;
+    });
   } catch (error) {
     return console.log(error);
   }
