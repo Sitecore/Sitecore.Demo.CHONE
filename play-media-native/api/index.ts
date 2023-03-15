bba45d37917139c23f0d38ea4b1991b2abe4ea8c
Christos Papoutsakis <96472119+chpa7@users.noreply.github.com>
2023-03-15 14:25:53 +0200

    AB#8577 PLAY! Media Native - Simplify initial connection validation (#94)

--- play-media-native/api/index.ts
+++ play-media-native/api/index.ts
@@ -5,8 +5,8 @@ type GraphQLResponseWithErrors = {
   errors: unknown[];
 };
 
-// API key and Preview URL are by default provided by the Redux store.
-// Optionally, fetchGrpahQL accepts an option argument, which is useful when validating CH1 connections.
+// API key and Preview URL are by default provided by Expo Secure Store.
+// Optionally, fetchGrpahQL accepts an options argument, which is useful when validating CH1 connections.
 // Preview URL and API key are passed in as parameters, to validate the fields provided.
 //
 export async function fetchGraphQL(query: string, options?: FetchOptions): Promise<unknown> {
@@ -23,24 +23,27 @@ export async function fetchGraphQL(query: string, options?: FetchOptions): Promi
         'X-GQL-Token': apiKey,
       },
       body: JSON.stringify({ query }),
-    })
-      .then((response: Response) => {
-        const jsonResponsePromise = response.json();
-        jsonResponsePromise.then((jsonResponse: unknown) => {
-          const responseWithErrors = jsonResponse as GraphQLResponseWithErrors;
-          if (responseWithErrors.errors && responseWithErrors.errors.length > 0) {
-            console.error(
-              'An error was returned by a GraphQL query. See the associated logged object for details.',
-              responseWithErrors
-            );
-          }
-        });
-        return jsonResponsePromise;
-      })
-      .catch((error) => {
-        return console.log(error);
+    }).then((response: Response) => {
+      if (!response.ok) {
+        console.error(
+          `GraphQL query failed with error ${response?.status} ${response?.statusText}`
+        );
+        throw response?.status;
+      }
+
+      const jsonResponsePromise = response.json();
+      jsonResponsePromise.then((jsonResponse: unknown) => {
+        const responseWithErrors = jsonResponse as GraphQLResponseWithErrors;
+        if (responseWithErrors.errors && responseWithErrors.errors.length > 0) {
+          console.error(
+            'An error was returned by a GraphQL query. See the associated logged object for details.',
+            responseWithErrors
+          );
+        }
       });
+      return jsonResponsePromise;
+    });
   } catch (error) {
-    return console.log(error);
+    throw error;
   }
 }
