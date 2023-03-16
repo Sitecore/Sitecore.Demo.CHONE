87821236d6ea5276583a633bab3da625eecda274
Christos Papoutsakis <chpa@sitecore.net>
2023-03-16 18:26:33 +0200

    Update media related screens

--- play-media-native/screens/AddCH1Media/AddCH1Media.tsx
+++ play-media-native/screens/AddCH1Media/AddCH1Media.tsx
@@ -2,6 +2,7 @@ import { useCallback, useMemo, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { Button } from 'react-native-paper';
 
+import { ListingCH1Media } from './ListingCH1Media';
 import { BottomActions } from '../../components/BottomActions/BottomActions';
 import { DropdownItem } from '../../components/DropdownPicker/DropdownPicker';
 import { SimpleFilters } from '../../components/FacetFilters/SimpleFilters';
@@ -17,7 +18,6 @@ import { useFilters } from '../../hooks/useFilters/useFilters';
 import { useMediaQuery } from '../../hooks/useMediaQuery/useMediaQuery';
 import { Media } from '../../interfaces/media';
 import { styles } from '../../theme/styles';
-import { ListingCH1Media } from './ListingCH1Media';
 
 export const AddCH1MediaScreen = ({ navigation, route }) => {
   const { contentItems, edit } = useContentItems();
@@ -33,6 +33,7 @@ export const AddCH1MediaScreen = ({ navigation, route }) => {
   const initialRoute = route?.params?.initialRoute;
   const single = route?.params?.single;
   const stateKey = route?.params?.stateKey;
+  const headerTitle = route?.params?.title;
 
   const availableImages = useMemo(() => {
     if (!images?.length) {
@@ -103,9 +104,10 @@ export const AddCH1MediaScreen = ({ navigation, route }) => {
 
       navigation.navigate(initialRoute, {
         isEditMedia: false,
+        title: headerTitle,
       });
     },
-    [edit, initialRoute, navigation, route.params.key, stateKey]
+    [edit, headerTitle, initialRoute, navigation, route.params.key, stateKey]
   );
 
   const onAdd = useCallback(() => {
