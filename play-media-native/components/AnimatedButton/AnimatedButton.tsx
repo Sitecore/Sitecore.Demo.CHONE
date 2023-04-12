8e1895ab08722516c60f2c928b2e72d69f94a847
Neli Kostadinova <nko@sitecore.net>
2023-04-12 09:09:34 +0300

    Custom animated button on all listing and detail screens

--- play-media-native/components/AnimatedButton/AnimatedButton.tsx
+++ play-media-native/components/AnimatedButton/AnimatedButton.tsx
@@ -1,9 +1,24 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { StyleProp, ViewStyle } from 'react-native';
 import { TouchableRipple, Text } from 'react-native-paper';
 
 import { theme } from '../../theme/theme';
 import { Icon } from '../Icon/Icon';
+import { MaterialIcon } from '../Icon/MaterialIcon';
 
-export const AnimatedButton = ({ iconName, label, onPress, style, extended }) => {
+export const AnimatedButton = ({
+  extended,
+  iconName,
+  label,
+  onPress,
+  style,
+}: {
+  extended: boolean;
+  iconName: keyof typeof MaterialCommunityIcons.glyphMap;
+  label: string;
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
+}) => {
   return (
     <TouchableRipple
       onPress={onPress}
@@ -14,12 +29,13 @@ export const AnimatedButton = ({ iconName, label, onPress, style, extended }) =>
           backgroundColor: theme.colors.yellow.DEFAULT,
           padding: theme.spacing.xs,
           overflow: 'hidden',
+          borderRadius: theme.spacing.xs,
         },
         style,
       ]}
     >
       <>
-        <Icon name={iconName} size={theme.sizing.animatedFabIconSize} />
+        <MaterialIcon name={iconName} size={theme.sizing.animatedFabIconSize} />
         {extended && (
           <Text
             variant="labelLarge"
--- play-media-native/hooks/useScrollOffset/useScrollOffset.ts
+++ play-media-native/hooks/useScrollOffset/useScrollOffset.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-// import { LayoutAnimation } from 'react-native';
+import { LayoutAnimation } from 'react-native';
 
 export const useScrollOffset = (initialState: boolean) => {
   const [isTopEdge, setIsTopEdge] = useState(initialState);
@@ -7,9 +7,9 @@ export const useScrollOffset = (initialState: boolean) => {
   const calcScrollOffset = ({ nativeEvent }) => {
     const currentScrollPosition = Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
 
-    // if (isTopEdge !== currentScrollPosition <= 0) {
-    //   LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-    // }
+    if (isTopEdge !== currentScrollPosition <= 0) {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    }
 
     setIsTopEdge(currentScrollPosition <= 0);
   };
