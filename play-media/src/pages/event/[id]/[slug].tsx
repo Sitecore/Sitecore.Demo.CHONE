3513a4280840bd3ef9e462d8b9e3374bad449f93
mfronczakdm <mfronczak1@gmail.com>
2023-04-17 17:00:24 -0400

    update map

--- play-media/src/pages/event/[id]/[slug].tsx
+++ play-media/src/pages/event/[id]/[slug].tsx
@@ -6,6 +6,7 @@ import EventDetailsPage from '../../../components/Pages/EventDetailsPage';
 import { REVALIDATE_INTERVAL } from '../../../constants/build';
 
 import { identifyVisitor, logViewEvent} from '../../../services/CdpService';
+import MapboxMap from "../../../components/mapbox/Mapbox";
 
 export interface Params {
   id: string;
