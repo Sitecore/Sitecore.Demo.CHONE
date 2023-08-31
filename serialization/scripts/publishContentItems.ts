02876e096a9b264296cfbaef637f20b40fa58a13
Jeff L'Heureux <jel@sitecore.net>
2023-08-31 11:35:49 -0400

    Add content types to the content item publishing command.

--- serialization/scripts/publishContentItems.ts
+++ serialization/scripts/publishContentItems.ts
@@ -11,12 +11,16 @@ const walk = (dir: fs.PathLike) => {
     if (stat && stat.isDirectory()) {
       results = results.concat(walk(file));
     } else {
-      results.push(
-        file
+      results.push({
+        contentItemID: file
           .split(/(\\|\/)/g)
           .pop()
-          ?.split('.')[0]
-      );
+          ?.split('.')[0],
+        contentType: dir
+          .toString()
+          .split(/(\\|\/)/g)
+          .pop()
+      });
     }
   });
   return results;
@@ -24,7 +28,7 @@ const walk = (dir: fs.PathLike) => {
 const contentItemIDs = walk(`${__dirname}/../items/contentItems`);
 
 // Publish each content item
-contentItemIDs.forEach((contentItemID) => {
-  console.log(`Publishing of content item '${contentItemID}' initiated`);
-  exec(`ch-one-cli delivery content publish -i ${contentItemID}`);
+contentItemIDs.forEach((contentItem: any) => {
+  console.log(`Publishing of '${contentItem.contentType}' content item '${contentItem.contentItemID}' initiated`);
+  exec(`ch-one-cli delivery content publish -i ${contentItem.contentItemID} -c ${contentItem.contentType}`);
 });
