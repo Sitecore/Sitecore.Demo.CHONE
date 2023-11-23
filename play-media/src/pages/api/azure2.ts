1fb0aa19337eb10f7fc0c88b093bd27d16db0bd6
soreilly6 <sarah.oreilly@sitecore.com>
2023-11-23 10:51:49 +0000

    updated chatGPT connection

--- play-media/src/pages/api/azure2.ts
+++ play-media/src/pages/api/azure2.ts
@@ -3,7 +3,7 @@ const apiKey = process.env.NEXT_PUBLIC_AZURE_OPENAI_KEY;
 const base_url = process.env.NEXT_PUBLIC_BASE_URL;
 const deploymentName = process.env.NEXT_PUBLIC_DEPLOYMENT_NAME;
 
-let url = `${base_url}/openai/deployments/${deploymentName}/completions?api-version=2022-12-01`;
+let url = `${base_url}/openai/deployments/${deploymentName}/chat/completions?api-version=2023-07-01-preview`;
 
 export default async function (req, res) {
     try {
@@ -16,17 +16,26 @@ export default async function (req, res) {
             },
 
             body: JSON.stringify({
-              model: "text-davinci-003",
-              prompt: "Topic: Hiking",
-              max_tokens: 250,
-              temperature: 0.7
-
+                model: "text-davinci-003",
+                messages: [
+                    {
+                        role: "user",
+                        content: "a short article summary for hiking in Epping Forest"
+                    }
+                ],
+                temperature: 0.5,
+                top_p: 1,
+                n: 1,
+                stream: false,
+                max_tokens: 150,
+                presence_penalty: 0,
+                frequency_penalty: 0
             }),
 
         });
 
         const data = await response.json();
-        res.status(200).json({ result: data.choices[0].text });
+        res.status(200).json({ result: data.choices[0].message.content });
 
 
     } catch(e) {
